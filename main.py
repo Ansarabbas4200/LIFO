f07f6b83f6080b019760b0f68cc6eaa74f27a881
Ansar Abbas <ansarabbas4200@gmail.com>
2023-02-20 11:08:06 +0500

    LIFO

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import queue
+
 q = queue.LifoQueue()
+# Size of the queue
 size_of_queue = input("How many items you want to add in Queue?\n")
 if size_of_queue.isdigit():
-    for x in range(int(size_of_queue)):
-        q.put(x+1)
+    size_of_queue = int(size_of_queue)
+    # Adds Items to the queue
+    for x in range(size_of_queue):
+        q.put(x + 1)
+    # Displays Items in the queue
     print("Members of the queue:")
     for n in list(q.queue):
         print(n, end="  ")
-removing_choice = input("\nHow many items you want to remove from Queue?\n")
-if removing_choice.isdigit():
-    for x in range(int(removing_choice)):
-        q.get()
+while True:
+    removing_choice = input("\nHow many items you want to remove from Queue? to Press 'E' to exit.\n").lower()
+    if q.qsize() == 0 or removing_choice == 'e':
+        break
+    if removing_choice.isdigit():
+        removing_choice = int(removing_choice)
+        if removing_choice > size_of_queue:
+            print("Enter a smaller number. ")
+            continue
+        # Removes Items from the Queue
+        for x in range(int(removing_choice)):
+            q.get()
         print("Members of the queue:")
-    for n in list(q.queue):
-        print(n, end="  ")
-    print("\nSize of the queue:")
-    print(q.qsize())
-else:
-    print("Enter a valid input!")
+        for n in list(q.queue):
+            print(n, end="  ")
+        print("\nSize of the queue:")
+        print(q.qsize())
+
+    else:
+        print("Enter a valid input!")
