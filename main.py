3d6bb55efa2d1c23ce676ceaa3ba54e61ca698ac
Ansar Abbas <ansarabbas4200@gmail.com>
2023-02-20 10:43:35 +0500

    LIFO

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import queue
 q = queue.LifoQueue()
-size_of_queue = int(input("How many items you want to add in Queue?\n"))
-for x in range(size_of_queue):
-    q.put(x+1)
-print("Members of the queue:")
-for n in list(q.queue):
-    print(n, end="  ")
-removing_choice = int(input("\nHow many items you want to remove from Queue?\n"))
-for x in range(removing_choice):
-    q.get()
-print("Members of the queue:")
-for n in list(q.queue):
-    print(n, end="  ")
-print("\nSize of the queue:")
-print(q.qsize())
+size_of_queue = input("How many items you want to add in Queue?\n")
+if size_of_queue.isdigit():
+    for x in range(int(size_of_queue)):
+        q.put(x+1)
+    print("Members of the queue:")
+    for n in list(q.queue):
+        print(n, end="  ")
+removing_choice = input("\nHow many items you want to remove from Queue?\n")
+if removing_choice.isdigit():
+    for x in range(int(removing_choice)):
+        q.get()
+        print("Members of the queue:")
+    for n in list(q.queue):
+        print(n, end="  ")
+    print("\nSize of the queue:")
+    print(q.qsize())
+else:
+    print("Enter a valid input!")
